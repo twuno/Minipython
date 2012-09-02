4e83a638e3426c4b2b682b36859ef4e529f401de
walter suazo <walter_suazo2002@hotmail.com>
2012-09-02 03:06:57 -0600

    modif 1/9/12

--- src/Expr/BinaryExp.java
+++ src/Expr/BinaryExp.java
@@ -5,18 +5,48 @@
 package Expr;
 
 import ASTNODE.ASTNode;
+import Expr.Expr;
 
 /**
  *
  * @author uno
  */
-public class BinaryExp extends Expr{
-    ASTNode expr1;
-    ASTNode expr2;
-    
-    @Override
-    public String toString() {
-        throw new UnsupportedOperationException("Not supported yet.");
+public abstract class BinaryExp extends Expr{
+    private ASTNode expr1;
+    private ASTNode expr2;
+
+    /**
+     * @return the expr1
+     */
+    public BinaryExp(ASTNode expr1, ASTNode expr2)
+    {
+        this.expr1=expr1;
+        this.expr2=expr2;
+    }
+    public ASTNode getExpr1() {
+        return expr1;
+    }
+
+    /**
+     * @param expr1 the expr1 to set
+     */
+    public void setExpr1(ASTNode expr1) {
+        this.expr1 = expr1;
+    }
+
+    /**
+     * @return the expr2
+     */
+    public ASTNode getExpr2() {
+        return expr2;
+    }
+
+    /**
+     * @param expr2 the expr2 to set
+     */
+    public void setExpr2(ASTNode expr2) {
+        this.expr2 = expr2;
     }
     
-}
+    
+ }
